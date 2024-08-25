69def625f43a1255e3b1567c76e9ddcf0daeff55
jodarini <rivasjose9428@gmail.com>
2024-08-25 15:25:14 -0500

    style: main nav

--- components/ui/nav/MainNav.vue
+++ components/ui/nav/MainNav.vue
@@ -14,32 +14,39 @@ const colorMode = useColorMode();
 
 <template>
   <nav
-    :class="cn('flex items-center space-x-4 lg:space-x-6', $attrs.class ?? '')"
+    :class="
+      cn(
+        'flex items-center space-x-4 justify-between lg:space-x-6',
+        $attrs.class ?? '',
+      )
+    "
   >
-    <a
-      href="/examples/dashboard"
-      class="text-sm font-medium transition-colors hover:text-primary"
-    >
-      Overview
-    </a>
-    <a
-      href="/examples/dashboard"
-      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-    >
-      Customers
-    </a>
-    <a
-      href="/examples/dashboard"
-      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-    >
-      Products
-    </a>
-    <a
-      href="/examples/dashboard"
-      class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-    >
-      Settings
-    </a>
+    <div class="space-x-4 lg:space-x-6">
+      <a
+        href="/examples/dashboard"
+        class="text-sm font-medium transition-colors hover:text-primary"
+      >
+        Overview
+      </a>
+      <a
+        href="/examples/dashboard"
+        class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+        Customers
+      </a>
+      <a
+        href="/examples/dashboard"
+        class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+        Products
+      </a>
+      <a
+        href="/examples/dashboard"
+        class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+        Settings
+      </a>
+    </div>
 
     <DropdownMenu>
       <DropdownMenuTrigger as-child>

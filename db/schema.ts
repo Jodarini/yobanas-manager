34bb52f2a8726fc5283a23b9b6acdc14701fe758
jodarini <rivasjose9428@gmail.com>
2025-05-29 14:20:39 -0500

    feat(validation): add product zod schema

--- db/schema.ts
+++ db/schema.ts
@@ -6,6 +6,7 @@ import {
   numeric,
   integer,
 } from 'drizzle-orm/pg-core';
+import z from 'zod';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey().notNull(),
@@ -47,3 +48,32 @@ export type SelectProductWithVariant = {
   variantInfo: SelectProductVariants;
   productInfo: SelectProduct;
 };
+
+export const insertProductSchema = z.object({
+  productInfo: z.object({
+    title: z.string().min(1, 'Debe ingresar un titulo'),
+    description: z.string().optional().nullable(),
+    price: z
+      .number({ message: 'Debe ser un numero' })
+      .positive('El precio debe ser positivo'),
+    category: z
+      .array(z.string())
+      .min(1, 'Debe agregar al menos una categoria')
+      .max(3, 'El producto debe tener maximo 3 categorias'),
+    thumbnail: z
+      .string()
+      .url('Debe ingresar un enlace correcto')
+      .optional()
+      .nullable(),
+    brand: z.string().min(1, 'La marca debe tener al menos un caracter'),
+    tags: z.string().optional().nullable(),
+  }),
+  variantInfo: z.object({
+    size: z.string(),
+    color: z.string().min(1, 'Debe agregar al menos un color'),
+    stock: z
+      .number({ message: 'Debe ser un numero' })
+      .int()
+      .nonnegative('La cantidad debe ser positiva'),
+  }),
+});

f31b0a095f371d934205b320131422325d879ffb
Jose David Rivas Benoit <rivasjose9428@gmail.com>
2025-10-24 14:45:34 -0500

    fix(UI): import missing icons

--- app/components/ProductForm.vue
+++ app/components/ProductForm.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils';
 import { useForm } from 'vee-validate';
-import { Trash2Icon, PlusCircleIcon } from 'lucide-vue-next';
+import { Trash2Icon, PlusCircleIcon, Check, Plus, ChevronsUpDown } from 'lucide-vue-next';
 import { toTypedSchema } from '@vee-validate/zod';
 import type {
   insertProductSchema,

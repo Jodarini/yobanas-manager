c13c5ff5daeaec175dcf475ea7d8fd8ff9428eaf
Jose David Rivas Benoit <rivasjose9428@gmail.com>
2025-10-29 17:45:53 -0500

    WIP: sales page, fetch variants on click

--- app/pages/add-sale.vue
+++ app/pages/add-sale.vue
@@ -1,27 +1,53 @@
 <script setup lang="ts">
 import { refDebounced } from '@vueuse/core'
+import type { Product } from '~~/db/schema'
 
 const searchTerm = ref('')
 const debouncedSearchTerm = refDebounced(searchTerm, 500) // 300ms debounce
+const selectedProduct = ref<Product>()
+const selectedProductId = computed(() => selectedProduct.value?.id)
+
 
 const { data, pending, error, refresh } =
   useFetch(() => `/api/products?search=${debouncedSearchTerm.value}`, {
     watch: [debouncedSearchTerm],
-    // lazy: true,
+    lazy: true,
   })
+
+const { data: variants, pending: variantsPending } = useFetch(
+  () => `/api/product/${selectedProductId.value}/variants`,
+  {
+    watch: [selectedProductId],
+    lazy: true, // Use lazy instead of immediate: false
+  }
+)
+
+watch(data, () =>
+  selectedProduct.value = data.value[0]
+)
 </script>
 
 <template>
   <Card>
-    <CardHeader>
+    <CardHeader class="border-b">
       <CardTitle>Agregar venta</CardTitle>
       <CardDescription>Agregar una venta a tu inventario</CardDescription>
     </CardHeader>
     <CardContent>
-      <div class="flex flex-col gap-4">
-        <Input placeholder="Buscar producto..." v-model="searchTerm" />
-        <div v-for="product in data">
-          {{ product.title }}
+      <div class="flex flex-row gap-4">
+        <div class="flex flex-col gap-4 max-w-1/3 border-r pr-4">
+          <Input placeholder="Buscar producto..." v-model="searchTerm" />
+          <div v-for="product in data" @click="selectedProduct = product">
+            {{ product.title }}
+          </div>
+        </div>
+        <div class="flex flex-col gap-4">
+          <h2 class="text-2xl">{{ selectedProduct?.title }}</h2>
+          <div v-for="variant in variants" :key="variant.id">
+            {{ variant.size }}
+            {{ variant.color }}
+            {{ variant.stock }}
+          </div>
         </div>
       </div>
 
---
+++ server/api/product/[id]/variants.ts
@@ -0,0 +1,24 @@
+import { serverSupabaseClient } from '#supabase/server';
+import { eq } from 'drizzle-orm';
+import { productVariants } from '~~/db/schema';
+
+export default defineEventHandler(async (event) => {
+
+  const id = getRouterParam(event, 'id');
+  const supabase = await serverSupabaseClient(event);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw createError({ statusCode: 401, statusMessage: 'Unauthorized' });
+  }
+
+  return await useAuthDB(user, async (db) => {
+
+    return await db
+      .select()
+      .from(productVariants)
+      .where(eq(productVariants.productId, Number(id)));
+  });
+})

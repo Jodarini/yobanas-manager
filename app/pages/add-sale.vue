b390dbf31db5bc27ed1cd61ccd46c30492f11ec7
Jose David Rivas Benoit <rivasjose9428@gmail.com>
2025-11-01 13:26:10 -0500

    WIP(feat): sales page responsive

--- app/pages/add-sale.vue
+++ app/pages/add-sale.vue
@@ -162,7 +162,7 @@ function goToItem(item: string, product?: Product) {
 </script>
 
 <template>
-  <Card class="min-h-screen lg:min-h-2/3">
+  <Card class="min-h-screen lg:min-h-2/3 lg:hidden">
     <form class="w-full h-full flex flex-col" @submit="onSubmit">
       <CardHeader class="border-b shrink-0">
         <CardTitle>Agregar venta</CardTitle>
@@ -423,6 +423,108 @@ function goToItem(item: string, product?: Product) {
           </Accordion>
         </div>
       </CardContent>
+    </form>
+  </Card>
+
+  <Card class="hidden min-h-2/3 lg:flex">
+    <form class="w-full space-y-8 h-full flex flex-col" @submit="onSubmit">
+      <CardHeader class="border-b">
+        <CardTitle>Agregar venta</CardTitle>
+        <CardDescription>Agregar una venta a tu inventario</CardDescription>
+      </CardHeader>
+      <CardContent class="flex-1 min-h-0">
+        <div class="flex flex-row gap-4 h-full">
+          <div class="flex flex-col gap-4 max-w-1/3 border-r pr-4">
+            <Input placeholder="Buscar producto..." v-model="searchTerm" />
+            <div>
+              <div class="hover:cursor-pointer hover:bg-accent p-2 rounded-sm" v-for="product in data"
+                @click="selectedProduct = product" :class="{ 'bg-accent': selectedProduct?.id === product.id }">
+                {{ product.title }}
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col gap-4 w-full min-h-0">
+            <div class="flex-1 min-h-0 overflow-y-auto">
+              <div v-if="selectedProduct" class="border-b pb-4 mb-4">
+                <h2 class="text-2xl">{{ selectedProduct?.title }}</h2>
+                <span class="text-muted-foreground">Agrega las variantes para la venta</span>
+              </div>
+
+              <div class="flex flex-col gap-4">
+                <div v-for="variant in variants" :key="variant.id" class="grid grid-cols-4 gap-4 text-muted-foreground">
+                  <span>Tamaño: {{ variant.size }} </span>
+                  <span>Color: {{ variant.color }}</span>
+                  <span>Stock: {{ variant.stock }}</span>
+                  <div class="flex flex-row gap-2">
+                    <NumberField v-model="variantStockToAdd[variant.id]" :default-value="1" :min="1"
+                      :max="variant.stock">
+                      <Label hidden>Stock</Label>
+                      <NumberFieldContent>
+                        <NumberFieldDecrement />
+                        <NumberFieldInput />
+                        <NumberFieldIncrement />
+                      </NumberFieldContent>
+                    </NumberField>
+                    <Button @click="handleAddToCart(selectedProduct, variant, variantStockToAdd[variant.id])"
+                      class="text-foreground">
+                      Agregar
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="border-t w-full overflow-y-auto min-h-2/6 max-h-2/6">
+              <h3 class="text-2xl bolder">Resumen de transacciones</h3>
+              <div v-for="prod in cartStore.cart" :key="prod.id" class="py-2 px-4">
+                <div class="flex flex-row justify-between">
+                  <div>
+                    <p>
+                      <span class="font-bold">{{ prod.title }} </span>
+                      - {{ prod.variant.size }} - {{ prod.variant.color }}
+                    </p>
+                    <p class="text-muted-foreground">
+                      Cantidad: {{ prod.stock }}
+                      <NumberField :default-value="1" :min="1" :max="prod.variant.stock">
+                        <Label hidden>Stock</Label>
+                        <NumberFieldContent>
+                          <NumberFieldDecrement />
+                          <NumberFieldInput />
+                          <NumberFieldIncrement />
+                        </NumberFieldContent>
+                      </NumberField>
+                    </p>
+                  </div>
+                  <div class="flex flex-row gap-2 items-center">
+                    <p>
+                      {{ (prod.price * prod.stock).toLocaleString('es-CO', {
+                        style: 'currency',
+                        currency: 'COP',
+                        minimumFractionDigits: 0
+                      }) }}
+                    </p>
+                    <Button @click="cartStore.removeItem(prod.variant.id)" variant="destructive">
+                      <TrashIcon />
+                    </Button>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter class="border-t flex flex-row justify-between">
+        Total: {{ cartStore.totalPrice.toLocaleString('es-CO', {
+          style: 'currency',
+          currency: 'COP',
+          minimumFractionDigits: 0
+        }) }}
+        <div class="flex flex-row gap-2">
+          <Button @click="cartStore.emptyCart" variant="outline">Cancelar</Button>
+          <Button>Completar venta</Button>
+        </div>
+      </CardFooter>
     </form>
   </Card>
 </template>

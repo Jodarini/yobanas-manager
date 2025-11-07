c489ad136e6817e74b09c66e9640415634821c82
Jose David Rivas Benoit <rivasjose9428@gmail.com>
2025-11-06 23:17:24 -0500

    config: supabase migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   schema: './db/schema.ts',
   dialect: 'postgresql',
+  out: './supabase/migrations',
   migrations: {
     prefix: 'supabase',
   },

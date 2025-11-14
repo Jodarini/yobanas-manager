777f432d7ece1efd1bf6a5cb032e0ce8f9280570
Jose David Rivas Benoit <rivasjose9428@gmail.com>
2025-11-14 16:18:38 -0500

    feat: sale pages

--- app/components/SalesTable.vue
+++ app/components/SalesTable.vue
@@ -1,213 +1,214 @@
 <script setup lang="ts">
-import type {
-  ColumnDef,
-  ColumnFiltersState,
-  ExpandedState,
-  SortingState,
-  VisibilityState,
-} from '@tanstack/vue-table';
-import {
-  FlexRender,
-  getCoreRowModel,
-  getExpandedRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useVueTable,
-} from '@tanstack/vue-table';
+  import type {
+    ColumnDef,
+    ColumnFiltersState,
+    ExpandedState,
+    SortingState,
+    VisibilityState,
+  } from '@tanstack/vue-table';
+  import {
+    FlexRender,
+    getCoreRowModel,
+    getExpandedRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useVueTable,
+  } from '@tanstack/vue-table';
 
-import { h, ref } from 'vue';
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
-// import { Checkbox } from '@/components/ui/checkbox';
-import { Button } from './ui/button';
-import { valueUpdater } from '@/lib/utils';
-import { ArrowUpDown, Badge } from 'lucide-vue-next';
-import type { Sale } from '~~/db/schema';
-import { NuxtLink } from '#components';
+  import { h, ref } from 'vue';
+  import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+  } from '@/components/ui/dropdown-menu';
+  import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from '@/components/ui/table';
+  // import { Checkbox } from '@/components/ui/checkbox';
+  import { Button } from './ui/button';
+  import { valueUpdater } from '@/lib/utils';
+  import { ArrowUpDown, Badge } from 'lucide-vue-next';
+  import type { Sale } from '~~/db/schema';
+  import { NuxtLink } from '#components';
 
-const props = defineProps<{
-  data: any[];
-}>();
+  const props = defineProps<{
+    data: any[];
+  }>();
 
-const columns: ColumnDef<Sale>[] = [
-  // {
-  //   id: 'select',
-  //   header: ({ table }) =>
-  //     h(Checkbox, {
-  //       modelValue:
-  //         table.getIsAllPageRowsSelected() ||
-  //         (table.getIsSomePageRowsSelected() && 'indeterminate'),
-  //       'onUpdate:modelValue': (value) =>
-  //         table.toggleAllPageRowsSelected(!!value),
-  //       ariaLabel: 'Select all',
-  //     }),
-  //   cell: ({ row }) =>
-  //     h(Checkbox, {
-  //       modelValue: row.getIsSelected(),
-  //       'onUpdate:modelValue': (value) => row.toggleSelected(!!value),
-  //       ariaLabel: 'Select row',
-  //     }),
-  //   enableSorting: false,
-  //   enableHiding: false,
-  // },
-  {
-    accessorKey: 'id',
-    header: 'ID',
-    cell: ({ row }) => h('div', { class: 'capitalize' }, row.getValue('id')),
-  },
-  {
-    accessorKey: 'status',
-    header: 'Estado',
-    cell: ({ row }) => {
-      const status = row.getValue('status');
-      const statusClasses = {
-        paid: 'bg-primary text-foreground',
-        pending: 'bg-yellow-100 text-yellow-800',
-        failed: 'bg-destructive text-foreground',
-      };
+  const columns: ColumnDef<Sale>[] = [
+    // {
+    //   id: 'select',
+    //   header: ({ table }) =>
+    //     h(Checkbox, {
+    //       modelValue:
+    //         table.getIsAllPageRowsSelected() ||
+    //         (table.getIsSomePageRowsSelected() && 'indeterminate'),
+    //       'onUpdate:modelValue': (value) =>
+    //         table.toggleAllPageRowsSelected(!!value),
+    //       ariaLabel: 'Select all',
+    //     }),
+    //   cell: ({ row }) =>
+    //     h(Checkbox, {
+    //       modelValue: row.getIsSelected(),
+    //       'onUpdate:modelValue': (value) => row.toggleSelected(!!value),
+    //       ariaLabel: 'Select row',
+    //     }),
+    //   enableSorting: false,
+    //   enableHiding: false,
+    // },
+    {
+      accessorKey: 'id',
+      header: 'ID',
+      cell: ({ row }) => h('div', { class: 'capitalize' }, row.getValue('id')),
+    },
+    {
+      accessorKey: 'status',
+      header: 'Estado',
+      cell: ({ row }) => {
+        const status = row.getValue('status');
+        const statusClasses = {
+          paid: 'bg-primary text-foreground',
+          pending: 'bg-yellow-100 text-yellow-800',
+          failed: 'bg-destructive text-foreground',
+        };
 
-      return h(
-        Badge,
-        { class: `capitalize ${statusClasses[status]}` },
-        () => row.getValue('status') // Wrap in arrow function
-      );
+        return h(
+          Badge,
+          { class: `capitalize ${statusClasses[status]}` },
+          () => row.getValue('status') // Wrap in arrow function
+        );
+      },
     },
-  },
-  {
-    accessorKey: 'created_at',
-    header: ({ column }) => {
-      return h(
-        Button,
-        {
-          variant: 'ghost',
-          onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
-        },
-        () => ['Fecha', h(ArrowUpDown, { class: 'ml-2 h-4 w-4' })]
-      );
+    {
+      accessorKey: 'created_at',
+      header: ({ column }) => {
+        return h(
+          Button,
+          {
+            variant: 'ghost',
+            onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+          },
+          () => ['Fecha', h(ArrowUpDown, { class: 'ml-2 h-4 w-4' })]
+        );
+      },
+      cell: ({ row }) => {
+        const date = new Date(row.getValue('created_at'));
+        const formatted = new Intl.DateTimeFormat('es-CO', {
+          day: 'numeric',
+          month: 'long',
+          year: 'numeric',
+        }).format(date);
+        return h('div', { class: 'lowercase' }, formatted);
+      },
     },
-    cell: ({ row }) => {
-      const date = new Date(row.getValue('created_at'));
-      const formatted = new Intl.DateTimeFormat('es-CO', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-      }).format(date);
-      return h('div', { class: 'lowercase' }, formatted);
-    },
-  },
-  {
-    accessorKey: 'total_amount',
-    header: ({ column }) => {
-      return h(
-        Button,
-        {
-          class: 'float-end',
-          variant: 'ghost',
-          onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
-        },
-        () => ['Total', h(ArrowUpDown, { class: 'ml-2 h-4 w-4' })]
-      );
-    },
-    cell: ({ row }) => {
-      const amount = Number.parseFloat(row.getValue('total_amount'));
+    {
+      accessorKey: 'total_amount',
+      header: ({ column }) => {
+        return h(
+          Button,
+          {
+            class: 'float-end',
+            variant: 'ghost',
+            onClick: () => column.toggleSorting(column.getIsSorted() === 'asc'),
+          },
+          () => ['Total', h(ArrowUpDown, { class: 'ml-2 h-4 w-4' })]
+        );
+      },
+      cell: ({ row }) => {
+        const amount = Number.parseFloat(row.getValue('total_amount'));
 
-      // Format the amount as a dollar amount
-      const formatted = new Intl.NumberFormat('es-CO', {
-        style: 'currency',
-        currency: 'COP',
-      }).format(amount);
+        // Format the amount as a dollar amount
+        const formatted = new Intl.NumberFormat('es-CO', {
+          style: 'currency',
+          currency: 'COP',
+        }).format(amount);
 
-      return h('div', { class: 'text-right font-medium' }, formatted);
+        return h('div', { class: 'text-right font-medium' }, formatted);
+      },
     },
+    {
+      id: 'actions',
+      enableHiding: false,
+      cell: ({ row }) => {
+        const sale = row.original;
 
-  },
-  {
-    id: 'actions',
-    enableHiding: false,
-    cell: ({ row }) => {
-      const sale = row.original;
+        return h(
+          'div',
+          { class: 'relative' },
+          h(
+            NuxtLink,
+            {
+              to: `/sales/${sale.id}`,
+            },
+            { default: () => 'Ver' }
+          )
+        );
+      },
+    },
+    // {
+    //   id: 'actions',
+    //   enableHiding: false,
+    //   cell: ({ row }) => {
+    //     const payment = row.original;
+    //
+    //     return h(
+    //       'div',
+    //       { class: 'relative' },
+    //       h(DropdownAction, {
+    //         payment,
+    //         onExpand: row.toggleExpanded,
+    //       })
+    //     );
+    //   },
+    // },
+  ];
 
-      return h(
-        'div',
-        { class: 'relative' },
-        h(NuxtLink, {
-          to: `/sale/${sale.id}`,
-        },
-          { default: () => 'Ver' }
-        )
-      );
-    },
-  },
-  // {
-  //   id: 'actions',
-  //   enableHiding: false,
-  //   cell: ({ row }) => {
-  //     const payment = row.original;
-  //
-  //     return h(
-  //       'div',
-  //       { class: 'relative' },
-  //       h(DropdownAction, {
-  //         payment,
-  //         onExpand: row.toggleExpanded,
-  //       })
-  //     );
-  //   },
-  // },
-];
+  const sorting = ref<SortingState>([]);
+  const columnFilters = ref<ColumnFiltersState>([]);
+  const columnVisibility = ref<VisibilityState>({});
+  const rowSelection = ref({});
+  const expanded = ref<ExpandedState>({});
 
-const sorting = ref<SortingState>([]);
-const columnFilters = ref<ColumnFiltersState>([]);
-const columnVisibility = ref<VisibilityState>({});
-const rowSelection = ref({});
-const expanded = ref<ExpandedState>({});
-
-const table = useVueTable({
-  get data() {
-    return props.data;
-  },
-  columns,
-  getCoreRowModel: getCoreRowModel(),
-  getPaginationRowModel: getPaginationRowModel(),
-  getSortedRowModel: getSortedRowModel(),
-  getFilteredRowModel: getFilteredRowModel(),
-  getExpandedRowModel: getExpandedRowModel(),
-  onSortingChange: (updaterOrValue) => valueUpdater(updaterOrValue, sorting),
-  onColumnFiltersChange: (updaterOrValue) =>
-    valueUpdater(updaterOrValue, columnFilters),
-  onColumnVisibilityChange: (updaterOrValue) =>
-    valueUpdater(updaterOrValue, columnVisibility),
-  state: {
-    get sorting() {
-      return sorting.value;
+  const table = useVueTable({
+    get data() {
+      return props.data;
     },
-    get columnFilters() {
-      return columnFilters.value;
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getExpandedRowModel: getExpandedRowModel(),
+    onSortingChange: (updaterOrValue) => valueUpdater(updaterOrValue, sorting),
+    onColumnFiltersChange: (updaterOrValue) =>
+      valueUpdater(updaterOrValue, columnFilters),
+    onColumnVisibilityChange: (updaterOrValue) =>
+      valueUpdater(updaterOrValue, columnVisibility),
+    state: {
+      get sorting() {
+        return sorting.value;
+      },
+      get columnFilters() {
+        return columnFilters.value;
+      },
+      get columnVisibility() {
+        return columnVisibility.value;
+      },
+      get rowSelection() {
+        return rowSelection.value;
+      },
+      get expanded() {
+        return expanded.value;
+      },
     },
-    get columnVisibility() {
-      return columnVisibility.value;
-    },
-    get rowSelection() {
-      return rowSelection.value;
-    },
-    get expanded() {
-      return expanded.value;
-    },
-  },
-});
+  });
 </script>
 
 <template>
@@ -229,14 +230,19 @@ const table = useVueTable({
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-          <DropdownMenuCheckboxItem v-for="column in table
-            .getAllColumns()
-            .filter((column) => column.getCanHide())" :key="column.id" class="capitalize"
-            :model-value="column.getIsVisible()" @update:model-value="
+          <DropdownMenuCheckboxItem
+            v-for="column in table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())"
+            :key="column.id"
+            class="capitalize"
+            :model-value="column.getIsVisible()"
+            @update:model-value="
               (value) => {
                 column.toggleVisibility(!!value);
               }
-            ">
+            "
+          >
             {{ column.id }}
           </DropdownMenuCheckboxItem>
         </DropdownMenuContent>
@@ -245,10 +251,16 @@ const table = useVueTable({
     <div class="border-border rounded-md">
       <Table>
         <TableHeader>
-          <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+          <TableRow
+            v-for="headerGroup in table.getHeaderGroups()"
+            :key="headerGroup.id"
+          >
             <TableHead v-for="header in headerGroup.headers" :key="header.id">
-              <FlexRender v-if="!header.isPlaceholder" :render="header.column.columnDef.header"
-                :props="header.getContext()" />
+              <FlexRender
+                v-if="!header.isPlaceholder"
+                :render="header.column.columnDef.header"
+                :props="header.getContext()"
+              />
             </TableHead>
           </TableRow>
         </TableHeader>
@@ -257,7 +269,10 @@ const table = useVueTable({
             <template v-for="row in table.getRowModel().rows" :key="row.id">
               <TableRow :data-state="row.getIsSelected() && 'selected'">
                 <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
-                  <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
+                  <FlexRender
+                    :render="cell.column.columnDef.cell"
+                    :props="cell.getContext()"
+                  />
                 </TableCell>
               </TableRow>
               <TableRow v-if="row.getIsExpanded()">
@@ -283,10 +298,20 @@ const table = useVueTable({
       <!--   {{ table.getFilteredRowModel().rows.length }} row(s) selected. -->
       <!-- </div> -->
       <div class="space-x-2">
-        <Button variant="outline" size="sm" :disabled="!table.getCanPreviousPage()" @click="table.previousPage()">
+        <Button
+          variant="outline"
+          size="sm"
+          :disabled="!table.getCanPreviousPage()"
+          @click="table.previousPage()"
+        >
           Anterior
         </Button>
-        <Button variant="outline" size="sm" :disabled="!table.getCanNextPage()" @click="table.nextPage()">
+        <Button
+          variant="outline"
+          size="sm"
+          :disabled="!table.getCanNextPage()"
+          @click="table.nextPage()"
+        >
           Siguiente
         </Button>
       </div>

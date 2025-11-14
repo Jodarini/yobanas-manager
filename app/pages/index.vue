ba054cdfe75e4471a91fc7daa21f16d9949dc06f
Jose David Rivas Benoit <rivasjose9428@gmail.com>
2025-11-14 17:13:17 -0500

    feat: breadcrumbs, products and product page organization

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,50 +1,89 @@
 <script setup lang="ts">
-  import { columns } from '@/components/columns';
-  const user = useSupabaseUser();
-
-  const { data, refresh, error, status } = await useFetch('/api/products', {
+  const { data, pending, error, refresh } = await useFetch('/api/products', {
     key: 'products',
     // getCachedData(key) {
     //   return useNuxtApp().payload.data[key] || useNuxtApp().static.data[key];
     // },
   });
+
+  const { data: dashboardData } = await useFetch('/api/dashboard/stats', {
+    key: 'stats',
+    // getCachedData(key) {
+    //   return useNuxtApp().payload.data[key] || useNuxtApp().static.data[key];
+    // },
+  });
+
+  const numProducts = computed(() => data.value.length);
+
+  const numBrands = computed(
+    () => new Set(data.value?.map((product) => product.brand)).size
+  );
 </script>
 
 <template>
   <div>
-    <template v-if="!user">
-      <h1 class="mb-6 text-2xl">Bienvenido!</h1>
-      <p>Inicia sesión para comenzar a crear productos.</p>
-      <Button as-child>
-        <NuxtLink to="/login">Iniciar sesión</NuxtLink>
-      </Button>
-    </template>
-    <template v-else-if="user">
-      <div class="flex justify-between">
-        <h1 class="mb-6 text-2xl">Sus productos</h1>
-      </div>
-
-      <div
-        v-if="status === 'pending'"
-        class="flex min-h-[400px] items-center justify-center"
-      >
-        <div class="flex flex-col items-center gap-4">
-          <div
-            class="h-12 w-12 animate-spin rounded-full border-b-2 border-gray-900"
-          />
-          <p class="text-gray-600">Cargando producto...</p>
-        </div>
-      </div>
-      <p v-else-if="error">{{ error }}</p>
-
-      <template v-else-if="status === 'success' && data && data.length > 0">
-        <Card>
-          <CardContent class="p-0 pt-0">
-            <DataTable :columns="columns" :data="data" />
+    <h1 class="mb-6 text-2xl">Dashboard</h1>
+    <div class="flex w-full flex-wrap gap-4">
+      <Card class="flex w-sm flex-row items-center justify-between">
+        <div class="w-full">
+          <CardContent class="text-2xl font-bold">
+            {{ numProducts }}
           </CardContent>
-        </Card>
-      </template>
-      <div v-else>¡Agrega nuevos items a tu inventario!</div>
-    </template>
+          <CardHeader>
+            <CardTitle class="font-medium">Referencias</CardTitle>
+          </CardHeader>
+        </div>
+        <CardFooter class="h-fit">
+          <Package />
+        </CardFooter>
+      </Card>
+
+      <Card class="flex w-sm flex-row items-center justify-between">
+        <div class="w-full">
+          <CardContent class="text-2xl font-bold">
+            {{ numBrands }}
+          </CardContent>
+          <CardHeader>
+            <CardTitle class="font-medium">Marcas</CardTitle>
+          </CardHeader>
+        </div>
+        <CardFooter class="h-fit">
+          <Tags />
+        </CardFooter>
+      </Card>
+
+      <Card class="flex w-sm flex-row items-center justify-between">
+        <div class="w-full">
+          <CardContent class="text-2xl font-bold">
+            {{ dashboardData?.stock }}
+          </CardContent>
+          <CardHeader>
+            <CardTitle class="font-medium">Productos en inventario</CardTitle>
+          </CardHeader>
+        </div>
+        <CardFooter class="h-fit">
+          <Warehouse />
+        </CardFooter>
+      </Card>
+      <Card class="flex w-sm flex-row items-center justify-between">
+        <div class="w-full">
+          <CardContent class="text-2xl font-bold">
+            {{
+              dashboardData?.value?.toLocaleString('es-CO', {
+                style: 'currency',
+                currency: 'COP',
+                minimumFractionDigits: 0,
+              })
+            }}
+          </CardContent>
+          <CardHeader>
+            <CardTitle class="font-medium">Valor total</CardTitle>
+          </CardHeader>
+        </div>
+        <CardFooter class="h-fit">
+          <DollarSign />
+        </CardFooter>
+      </Card>
+    </div>
   </div>
 </template>

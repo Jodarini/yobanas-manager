1d792b56477c3875f13311334bf15793f20b4673
Jose David Rivas Benoit <rivasjose9428@gmail.com>
2025-11-28 18:01:08 -0500

    style(sales): add card appearance to sales table

--- app/components/SalesTable.vue
+++ app/components/SalesTable.vue
@@ -226,109 +226,116 @@
 </script>
 
 <template>
-  <div class="w-full">
-    <div class="flex items-center gap-2 py-4">
-      <Input
-        class="max-w-sm"
-        placeholder="Filtrar ventas"
-        :model-value="table.getColumn('created_at')?.getFilterValue() as string"
-        @update:model-value="
-          table.getColumn('created_at')?.setFilterValue($event)
-        "
-      />
-      <DropdownMenu>
-        <DropdownMenuTrigger as-child>
-          <Button variant="outline" class="ml-auto">
-            Columnas
-            <ChevronDown class="ml-2 h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuCheckboxItem
-            v-for="column in table
-              .getAllColumns()
-              .filter((column) => column.getCanHide())"
-            :key="column.id"
-            class="capitalize"
-            :model-value="column.getIsVisible()"
-            @update:model-value="
-              (value) => {
-                column.toggleVisibility(!!value);
-              }
-            "
-          >
-            {{ column.id }}
-          </DropdownMenuCheckboxItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
-    <div class="border-border rounded-md">
-      <Table>
-        <TableHeader>
-          <TableRow
-            v-for="headerGroup in table.getHeaderGroups()"
-            :key="headerGroup.id"
-          >
-            <TableHead v-for="header in headerGroup.headers" :key="header.id">
-              <FlexRender
-                v-if="!header.isPlaceholder"
-                :render="header.column.columnDef.header"
-                :props="header.getContext()"
-              />
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <template v-if="table.getRowModel().rows?.length">
-            <template v-for="row in table.getRowModel().rows" :key="row.id">
-              <TableRow :data-state="row.getIsSelected() && 'selected'">
-                <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
-                  <FlexRender
-                    :render="cell.column.columnDef.cell"
-                    :props="cell.getContext()"
-                  />
-                </TableCell>
-              </TableRow>
-              <TableRow v-if="row.getIsExpanded()">
-                <TableCell :colspan="row.getAllCells().length">
-                  {{ JSON.stringify(row.original) }}
-                </TableCell>
-              </TableRow>
-            </template>
-          </template>
-
-          <TableRow v-else>
-            <TableCell :colspan="columns.length" class="h-24 text-center">
-              Sin ventas.
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </div>
-
-    <div class="flex items-center justify-end space-x-2 py-4">
-      <!-- <div class="text-muted-foreground flex-1 text-sm"> -->
-      <!--   {{ table.getFilteredSelectedRowModel().rows.length }} of -->
-      <!--   {{ table.getFilteredRowModel().rows.length }} row(s) selected. -->
-      <!-- </div> -->
-      <div class="space-x-2">
-        <Button
-          variant="outline"
-          size="sm"
-          :disabled="!table.getCanPreviousPage()"
-          @click="table.previousPage()"
-        >
-          Anterior
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          :disabled="!table.getCanNextPage()"
-          @click="table.nextPage()"
-        >
-          Siguiente
-        </Button>
+  <Card class="w-full">
+    <CardContent>
+      <div class="flex items-center gap-2 py-4">
+        <Input
+          class="max-w-sm"
+          placeholder="Filtrar ventas"
+          :model-value="
+            table.getColumn('created_at')?.getFilterValue() as string
+          "
+          @update:model-value="
+            table.getColumn('created_at')?.setFilterValue($event)
+          "
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger as-child>
+            <Button variant="outline" class="ml-auto">
+              Columnas
+              <ChevronDown class="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuCheckboxItem
+              v-for="column in table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())"
+              :key="column.id"
+              class="capitalize"
+              :model-value="column.getIsVisible()"
+              @update:model-value="
+                (value) => {
+                  column.toggleVisibility(!!value);
+                }
+              "
+            >
+              {{ column.id }}
+            </DropdownMenuCheckboxItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-    </div>
-  </div>
+      <div class="border-border rounded-md">
+        <Table>
+          <TableHeader>
+            <TableRow
+              v-for="headerGroup in table.getHeaderGroups()"
+              :key="headerGroup.id"
+            >
+              <TableHead v-for="header in headerGroup.headers" :key="header.id">
+                <FlexRender
+                  v-if="!header.isPlaceholder"
+                  :render="header.column.columnDef.header"
+                  :props="header.getContext()"
+                />
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <template v-if="table.getRowModel().rows?.length">
+              <template v-for="row in table.getRowModel().rows" :key="row.id">
+                <TableRow :data-state="row.getIsSelected() && 'selected'">
+                  <TableCell
+                    v-for="cell in row.getVisibleCells()"
+                    :key="cell.id"
+                  >
+                    <FlexRender
+                      :render="cell.column.columnDef.cell"
+                      :props="cell.getContext()"
+                    />
+                  </TableCell>
+                </TableRow>
+                <TableRow v-if="row.getIsExpanded()">
+                  <TableCell :colspan="row.getAllCells().length">
+                    {{ JSON.stringify(row.original) }}
+                  </TableCell>
+                </TableRow>
+              </template>
+            </template>
+
+            <TableRow v-else>
+              <TableCell :colspan="columns.length" class="h-24 text-center">
+                Sin ventas.
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+
+      <div class="flex items-center justify-end space-x-2 py-4">
+        <!-- <div class="text-muted-foreground flex-1 text-sm"> -->
+        <!--   {{ table.getFilteredSelectedRowModel().rows.length }} of -->
+        <!--   {{ table.getFilteredRowModel().rows.length }} row(s) selected. -->
+        <!-- </div> -->
+        <div class="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            :disabled="!table.getCanPreviousPage()"
+            @click="table.previousPage()"
+          >
+            Anterior
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            :disabled="!table.getCanNextPage()"
+            @click="table.nextPage()"
+          >
+            Siguiente
+          </Button>
+        </div>
+      </div>
+    </CardContent>
+  </Card>
 </template>

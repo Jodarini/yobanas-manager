be3c1189456313dcd600a642132f5f7e6675fed4
Jose David Rivas Benoit <rivasjose9428@gmail.com>
2025-12-03 00:30:04 -0500

    feat: add theme selector, add your subscription link

--- app/components/AppSidebar.vue
+++ app/components/AppSidebar.vue
@@ -1,52 +1,55 @@
 <script setup lang="ts">
-import {
-  Package,
-  DatabaseIcon,
-  SunIcon,
-  MoonIcon,
-  ShoppingCartIcon,
-  Store,
-} from 'lucide-vue-next';
+  import {
+    Package,
+    DatabaseIcon,
+    SunIcon,
+    MoonIcon,
+    ShoppingCartIcon,
+    Store,
+    MonitorIcon,
+  } from 'lucide-vue-next';
 
-const { user, signOut, isLoading } = useAuth();
+  const { user, signOut, isLoading } = useAuth();
 
-const route = useRoute();
+  const route = useRoute();
 
-const menuItems = [
-  {
-    title: 'Dashboard',
-    url: '/',
-    icon: DatabaseIcon,
-  },
-  {
-    title: 'Productos',
-    url: '/products',
-    icon: Package,
-  },
-  {
-    title: 'Carrito',
-    url: '/add-sale',
-    icon: ShoppingCartIcon,
-  },
-  {
-    title: 'Ventas',
-    url: '/sales',
-    icon: Store,
-  },
-];
+  const menuItems = [
+    {
+      title: 'Dashboard',
+      url: '/',
+      icon: DatabaseIcon,
+    },
+    {
+      title: 'Productos',
+      url: '/products',
+      icon: Package,
+    },
+    {
+      title: 'Carrito',
+      url: '/add-sale',
+      icon: ShoppingCartIcon,
+    },
+    {
+      title: 'Ventas',
+      url: '/sales',
+      icon: Store,
+    },
+  ];
 
-const isActive = (url: string) => {
-  return route.path === url || route.path.startsWith(url + '/');
-};
+  const isActive = (url: string) => {
+    return route.path === url || route.path.startsWith(url + '/');
+  };
 
-const colorMode = useColorMode();
+  const colorMode = useColorMode();
 </script>
 
 <template>
   <Sidebar>
     <SidebarHeader>
       <div class="flex items-center justify-between gap-2 px-4 py-2">
-        <div class="bg-primary text-primary-foreground flex h-8 w-8 items-center justify-center rounded-lg">
+        <div
+          class="bg-primary text-primary-foreground flex h-8 w-8 items-center justify-center rounded-lg"
+        >
           <Package class="h-4 w-4" />
         </div>
         <div class="flex flex-col">
@@ -57,22 +60,62 @@ const colorMode = useColorMode();
         <DropdownMenu>
           <DropdownMenuTrigger as-child>
             <Button variant="outline">
-              <SunIcon class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <SunIcon
+                class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"
+              />
               <MoonIcon
-                class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"
+              />
               <span class="sr-only">Toggle theme</span>
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
-            <DropdownMenuItem @click="colorMode.preference = 'light'">
-              Light
+            <DropdownMenuItem
+              class="focus:text-sidebar-foreground focus:bg-transparent"
+            >
+              Theme
+              <ToggleGroup type="single" class="bg-accent-foreground/10">
+                <ToggleGroupItem
+                  class="hover:text-foreground hover:bg-foreground/5"
+                  :class="{
+                    'bg-accent-foreground/30 focus:text-foreground-accent hover:bg-accent-foreground/35':
+                      colorMode.preference === 'light',
+                  }"
+                  value="light"
+                  aria-label="Toggle light"
+                  @click="colorMode.preference = 'light'"
+                >
+                  <SunIcon />
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="dark"
+                  aria-label="Toggle dark"
+                  class="hover:text-foreground hover:bg-foreground/5"
+                  :class="{
+                    'bg-accent-foreground/30 focus:text-foreground-accent hover:bg-accent-foreground/35':
+                      colorMode.preference === 'dark',
+                  }"
+                  @click="colorMode.preference = 'dark'"
+                >
+                  <MoonIcon />
+                </ToggleGroupItem>
+                <ToggleGroupItem
+                  value="system"
+                  aria-label="Toggle system"
+                  class="hover:text-foreground hover:bg-foreground/5"
+                  :class="{
+                    'bg-accent-foreground/30 focus:text-foreground-accent hover:bg-accent-foreground/35':
+                      colorMode.preference === 'system',
+                  }"
+                  @click="colorMode.preference = 'system'"
+                >
+                  <MonitorIcon />
+                </ToggleGroupItem>
+              </ToggleGroup>
             </DropdownMenuItem>
-            <DropdownMenuItem @click="colorMode.preference = 'dark'">
-              Dark
-            </DropdownMenuItem>
-            <DropdownMenuItem @click="colorMode.preference = 'system'">
-              System
-            </DropdownMenuItem>
+            <NuxtLink as-child to="/subscriptions">
+              <DropdownMenuItem>Tu subscripción</DropdownMenuItem>
+            </NuxtLink>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
@@ -101,7 +144,13 @@ const colorMode = useColorMode();
         <Button v-if="!user" class="min-w-full" as-child variant="outline">
           <NuxtLink to="/login">Iniciar sesión</NuxtLink>
         </Button>
-        <Button v-else :disabled="isLoading" variant="outline" class="flex min-w-full items-center" @click="signOut">
+        <Button
+          v-else
+          :disabled="isLoading"
+          variant="outline"
+          class="flex min-w-full items-center"
+          @click="signOut"
+        >
           <span v-if="isLoading" class="flex items-center">
             <Spinner class="mr-2" />
             Cerrando sesión...
@@ -117,7 +166,11 @@ const colorMode = useColorMode();
         <p class="text-muted-foreground text-xs">
           © 2025 StockIt
           <br />
-          <NuxtLink to="https://www.jodarini.dev" target="_blank" class="text-accent-foreground">
+          <NuxtLink
+            to="https://www.jodarini.dev"
+            target="_blank"
+            class="text-accent-foreground"
+          >
             by Jodarini.dev
           </NuxtLink>
         </p>
